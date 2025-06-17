2996b78b98e1e3272b2c62b07eef8f10e10df31d
Josiah Dalisay <dev@unmessy.io>
2025-06-17 17:26:06 +0800

    type  module

--- api/index.js
+++ api/index.js
@@ -2,8 +2,7 @@
 // This file serves as the entry point for Vercel serverless functions
 // It imports and re-exports the Express app from src/api/index.js
 
-// Use dynamic import to handle ES modules from CommonJS
-module.exports = async (req, res) => {
-  const { default: app } = await import('../src/api/index.js');
-  return app(req, res);
-};
+import app from '../src/api/index.js';
+
+// Export the Express app as the default export for Vercel
+export default app;
